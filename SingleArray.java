5f350f066b27c824b0d56fff9ab45505aa25e219
pulagalaalekhya <pulagalaalekhya@gmail.com>
2025-12-19 14:28:16 +0530

    Add files via upload

---
+++ SingleArray.java
@@ -0,0 +1,15 @@
+package com.avn.tns.oopsdemo;
+import java.util.Arrays;
+import java.util.Scanner;
+public class SingleArray{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int arr2[]=new int[5];
+        for(int i=0;i<arr2.length;i++){
+            if(sc.hasNextInt()){
+                arr2[i]=sc.nextInt();
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+}
---
+++ access Modifermain.java
@@ -0,0 +1,12 @@
+package com.avn.tns.modifer;
+
+public class accessModifermain {
+    public static void main(String[] args) {
+        accessModifer a=new accessModifer();
+        System.out.println(a.collegename);//public
+        System.out.println(a.branch);//default only within package
+        //System.out.println(a.id);private only within class
+        System.out.println(a.name);//protected only within package and outside package subclass thats extends this class
+    }
+
+}
